f06d8f41ad536764c182a50060a9fec8711ad13d
Pramit Das <daspramit92@gmail.com>
2026-03-19 21:31:06 +0530

    build error fixed

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,13 +1,13 @@
 import type { NextConfig } from "next";
 import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import path, { dirname } from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const nextConfig: NextConfig = {
   webpack: (config) => {
-    config.resolve.alias['@'] = __dirname;
+    config.resolve.alias['@'] = path.resolve(__dirname);
     return config;
   },
 };
